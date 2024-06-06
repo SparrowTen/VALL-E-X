80e19881d396dc1dd6c59dcbd8968c71d2d07358
benhsu0828 <benten.hsu@gmail.com>
2024-06-06 22:58:04 +0800

    批次生成

--- batchGenerate.py
+++ batchGenerate.py
@@ -1,31 +1,58 @@
 from utils.generation import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 from utils.prompt_making import make_prompt
-
 import os
+#進度條
+import time
+from tqdm import tqdm, trange
 
-# make_prompt(name="ayaka_prompt", audio_prompt_path="D:/逢甲大學/畢業專題/V33_Merged_Chinese_Wav/Merged_Chinese_Wav/VO_AQ/VO_ayaka/vo_DQAQ004_4_ayaka_04.wav")
-# preload_models()
-# audio_array = generate_audio(text_prompt, prompt="ayaka_prompt")
-# write_wav("./result/ayaka_cloned.wav", SAMPLE_RATE, audio_array)
-
+#簡體中文轉繁體中文
+from opencc import OpenCC
+cc = OpenCC('s2twp')
+#中文
 if __name__ == '__main__':
-    raw_audio_dir = "D:/逢甲大學/畢業專題/voice_data/「博士」/"
+    raw_audio_dir = "D:/逢甲大學/畢業專題/voice_data/「博士」-zh/"
     filelist = list(os.walk(raw_audio_dir))[0][2]
     preload_models()
-    #print(filelist[3])
-    #make_prompt(name="博士_prompt", audio_prompt_path=f"{raw_audio_dir}{filelist[3]}",transcript=f"{raw_audio_dir}{filelist[2]}")
-    count = 0
-    num = 0
-    for filename in filelist:
+    print(filelist[5])
+    with open(f"{raw_audio_dir}{filelist[4]}" , 'r', encoding='utf-8') as f:
+        make_prompt(name="博士-zh_prompt", audio_prompt_path=f"{raw_audio_dir}{filelist[5]}",transcript=cc.convert(f.read()))
+    count = 1
+    # num = 0
+    for filename in tqdm(filelist):
         print(filename)
-        print(f"目前筆數:{num}/{len(filelist)}")
-        num += 1
+        # print(f"目前筆數:{num}/{len(filelist)}")
+        # num += 1
         if filename.endswith('.lab'):
             with open(f"{raw_audio_dir}{filename}" , 'r', encoding='utf-8') as f:
                 text = f.read()
-                print(text)
+                print("轉換前: "+ text)
+                text = cc.convert(text)
+                print("轉換後: "+ text)
                 #print(f"File name: {raw_audio_dir}{filename}")
-                audio_array = generate_audio(text, prompt="博士_prompt",language='ja')
-                write_wav(f"./result/博士/博士_cloned{count}.wav", SAMPLE_RATE, audio_array)
+                audio_array = generate_audio(text, prompt="博士-zh_prompt")#,language='zh'
+                write_wav(f"./result/博士-zh/博士_cloned{count}.wav", SAMPLE_RATE, audio_array)
                 count += 1
+
+# #英文
+# if __name__ == '__main__':
+#     raw_audio_dir = "D:/逢甲大學/畢業專題/voice_data/「博士」-en/"
+#     filelist = list(os.walk(raw_audio_dir))[0][2]
+#     preload_models()
+#     #print(filelist[3])
+#     with open(f"{raw_audio_dir}{filelist[4]}" , 'r', encoding='utf-8') as f:
+#        make_prompt(name="博士-en_prompt", audio_prompt_path=f"{raw_audio_dir}{filelist[5]}",transcript=f.read())
+#     count = 0
+#     num = 0
+#     for filename in filelist:
+#         print(filename)
+#         print(f"目前筆數:{num}/{len(filelist)}")
+#         num += 1
+#         if filename.endswith('.lab'):
+#             with open(f"{raw_audio_dir}{filename}" , 'r', encoding='utf-8') as f:
+#                 text = f.read()
+#                 print(text)
+#                 #print(f"File name: {raw_audio_dir}{filename}")
+#                 audio_array = generate_audio(text, prompt="博士_prompt",language='en')
+#                 write_wav(f"./result/博士/博士-en_cloned{count}.wav", SAMPLE_RATE, audio_array)
+#                 count += 1
--- generate.py
+++ generate.py
@@ -1,12 +1,43 @@
 from utils.generation import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
+from utils.prompt_making import make_prompt
+import os
 
-# download and load all models
-preload_models()
 
-text_prompt = """
-这是历史的开始。 如果您想听更多，请继续。
-"""
-audio_array = generate_audio(text_prompt, prompt="博士_prompt")
+# from opencc import OpenCC
+# cc = OpenCC('s2twp')
+# if  __name__ == '__main__':
+#     # download and load all models
+#     preload_models()
+#     raw_audio_dir = "D:/逢甲大學/畢業專題/voice_data/「博士」-zh/"
+#     with open(f"{raw_audio_dir}vo_LLZAQ001_12_dottore_02.lab" , 'r', encoding='utf-8') as f:
+#         text_prompt = f.read()
+#         print("原始文本: " + text_prompt)
+#         text_prompt = cc.convert(text_prompt)
+#         print("轉換後: "+text_prompt)
 
-write_wav("./result/test_cloned.wav", SAMPLE_RATE, audio_array)
+#         #製作聲音特徵
+#         make_prompt(name="博士-zh_prompt", audio_prompt_path=f"{raw_audio_dir}vo_LLZAQ001_12_dottore_02.wav",transcript=text_prompt)
+
+#         #生成音訊
+#         audio_array = generate_audio(text_prompt, prompt="博士-zh_prompt")
+
+#         write_wav("./result/test_cloned.wav", SAMPLE_RATE, audio_array)
+
+
+#英文
+if __name__ == '__main__':
+    preload_models()
+    raw_audio_dir = "D:/逢甲大學/畢業專題/voice_data/「博士」-zh/"
+    with open(f"D:/逢甲大學/畢業專題/voice_data/「博士」-en/vo_LLZAQ001_12_dottore_07.lab" , 'r', encoding='utf-8') as f:
+        #製作聲音特徵
+        text_prompt = f.read()
+        make_prompt(name="博士-en_prompt", audio_prompt_path="D:/逢甲大學/畢業專題/voice_data/「博士」-en/vo_LLZAQ001_12_dottore_07.wav",transcript=text_prompt)
+
+    with open(f"{raw_audio_dir}vo_LLZAQ001_12_dottore_07.lab" , 'r', encoding='utf-8') as f:
+        text_prompt = f.read()
+        print("原始文本: " + text_prompt)
+
+        #生成音訊
+        audio_array = generate_audio(text_prompt, prompt="博士-en_prompt")
+        write_wav("./result/test_cloned-crossLingual-zh2en.wav", SAMPLE_RATE, audio_array)
